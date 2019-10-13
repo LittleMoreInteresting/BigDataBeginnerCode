e7d76223324b7da923a2ab9234d0400465552681
Herny <1208818417@qq.com>
2019-10-13 11:16:18 +0800

    1012

---
+++ src/Lesson_1012/FirstClass.java
@@ -0,0 +1,48 @@
+package Lesson_1012;
+
+/**
+ * @author Henry
+ */
+public class FirstClass {
+    public static void main(String[] args){
+        int level = 7;
+        String backslash = "\\";
+        String forwardSlash = "/";
+        String verticalLine = "|";
+        String underLine = "_";
+        String blankSpace = " ";
+       for(int i = 0; i<level+level; i++){
+            if(i<level){
+                for (int j=0; j<level-i; j++){
+                    System.out.print(blankSpace);
+                }
+                System.out.print(forwardSlash);
+                for (int j=0; j<i+i; j++){
+                    System.out.print(blankSpace);
+                }
+                System.out.print(backslash);
+                for (int j=0; j<level-i; j++){
+                    System.out.print(blankSpace);
+                }
+            }else if (i==level){
+                System.out.print(forwardSlash);
+                for (int j=0; j<level+level; j++){
+                    System.out.print(underLine);
+                }
+                System.out.print(backslash);
+            }else{
+                System.out.print(verticalLine);
+                for (int j=0; j<level+level; j++){
+                    System.out.print(blankSpace);
+                }
+                System.out.print(verticalLine);
+            }
+            System.out.print("\n");
+        }
+        System.out.print(verticalLine);
+        for (int j=0; j<level+level; j++){
+            System.out.print(underLine);
+        }
+        System.out.print(verticalLine);
+    }
+}
