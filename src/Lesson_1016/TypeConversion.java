9833f622f0bf8e30a4e071aff4d84790a2fe9277
shr26207 <sunhaoran@offcn.com>
2019-10-18 17:25:07 +0800

    1016作业

--- src/Lesson_1016/TypeConversion.java
+++ src/Lesson_1016/TypeConversion.java
@@ -7,15 +7,37 @@ public class TypeConversion {
         variableDouble = variableInt;
         System.out.println(variableInt);
         System.out.println(variableDouble);
+
+        byte byteVal = 99;
+        float floatVal;
+        floatVal = byteVal;
+        System.out.println(byteVal);
+        System.out.println(floatVal);
+
         /**
          * @ 强制类型转换
          */
         System.out.println("--------------");
         float variableFloat = 3.14159f;
         int float2Int;
+        /**
+         * @ float --> int
+         */
         float2Int = (int)variableFloat;
         System.out.println(variableFloat);
         System.out.println(float2Int);
+
+        /**
+         * @ float --> long
+         */
+        float varFloat = 999.14159f;
+        long float2Long = (long)varFloat;
+        System.out.println(float2Long);
+        /**
+         * @ long --> byte
+         */
+        byte long2Byte = (byte)float2Long;
+        System.out.println(long2Byte);
 
     }
 }
